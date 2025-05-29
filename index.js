8936d8e3f711e54d0bce8409a9c72a6decbf6286
lloydlijo5088 <nightowl5088@gmail.com>
2025-05-29 12:06:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 
 const bot = mineflayer.createBot({
-  host: 'moose.aternos.host',
+  host: 'candiru.aternos.host',
   port: 31369,
   username: 'AFK_Bot',
   auth: 'offline'
